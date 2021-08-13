2096fb1cccc59a8b8f3dc49a0a36eb84a6c72a9e
Raza Hassan <bilgramiraza@yahoo.com>
2021-08-13 17:05:34 +0530

    Added basic operations

---
+++ script.js
@@ -0,0 +1,37 @@
+function add(a,b){
+    return a+b;
+}
+function Subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    if(!a || !b)
+        return "Invalid";
+    return a/b;
+}
+
+function operate(){
+    let a=parseFloat(prompt("Enter the First Numbers"),10);
+    let b=parseFloat(prompt("Enter the First Numbers"),10);
+    let operator =prompt("Enter the Operation(+,-,*,/)");
+    let result;
+    switch (operator) {
+        case '+':
+            result=add(a,b);
+            break;
+        case '-':
+            result=Subtract(a,b);
+            break;
+        case '*':
+            result=multiply(a,b);
+            break;
+        case '/':
+            result=divide(a,b);
+            break;
+    }
+    console.log(result);
+}
+operate();
